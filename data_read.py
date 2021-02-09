f93d9c74592a15fe154ca214aea2ba63976f6131
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-02-09 22:39:28 +0100

    Data read complete, headers sorted out, DF converted to numerics.

--- data_read.py
+++ data_read.py
@@ -9,5 +9,18 @@ Created on Mon Feb  8 15:44:10 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#The data file was cleaned to remove the first couple of lines. 
 df = pd.read_csv('run001.txt', sep = '\t')
 df = df.drop([0])
+
+#all column heads are stored to recreate proper columns
+col_head = list(df.columns)
+#the data series is throwing an exception that the data is not numeric
+#the following for loop converts the entire dataframe to numeric which 
+#can then be plotted
+for i in range(len(df.columns)):
+    x = pd.to_numeric(df[col_head[i]])
+    df = df.drop(columns=[col_head[i]])
+    df.insert(i, col_head[i], x)
+
+
