2ae828ce158bad28dc47451b889a42a2e1820cc0
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-03-18 16:48:27 +0100

    The primary plotting functions completed. All 3 graphs can be plotted for multiple columns.

--- Dashboard.py
+++ Dashboard.py
@@ -16,7 +16,7 @@ import os
 import sys
 from pathlib import Path
 import data_import_func
-
+from scipy.stats import norm
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
@@ -47,24 +47,43 @@ class Window(QtWidgets.QWidget):
         self.plot_b2 = QtWidgets.QPushButton('Plot', self)
         self.plot_b2.pressed.connect(self.plot) 
 
+#display statistical information about the signal
+        self.l3 = QtWidgets.QLabel(self)
+        self.l3.setText('Average of Time Series')
+        self.l3_output = QtWidgets.QLineEdit(self)
+        
+        self.l4 = QtWidgets.QLabel(self)
+        self.l4.setText('Std of Time Series')
+        self.l4_output = QtWidgets.QLineEdit(self)
+        
+        self.l5 = QtWidgets.QLabel(self)
+        self.l5.setText('Max of Time Series')
+        self.l5_output = QtWidgets.QLineEdit(self)
+        
+        self.l6 = QtWidgets.QLabel(self)
+        self.l6.setText('Min of Time Series')
+        self.l6_output = QtWidgets.QLineEdit(self)
+        
+
+
         figure1 = Figure()
         figure2 = Figure()
         figure3 = Figure()
         figure4 = Figure()
-        canvas1 = FigureCanvas(figure1)
-        canvas2 = FigureCanvas(figure2)
-        canvas3 = FigureCanvas(figure3)
-        canvas4 = FigureCanvas(figure4)
+        self.canvas1 = FigureCanvas(figure1)
+        self.canvas2 = FigureCanvas(figure2)
+        self.canvas3 = FigureCanvas(figure3)
+        self.canvas4 = FigureCanvas(figure4)
         self.ax1 = figure1.add_subplot(111)
         self.ax2 = figure2.add_subplot(111)
         self.ax3 = figure3.add_subplot(111)
         self.ax4 = figure4.add_subplot(111)
         
         
-        toolbar1 = NavigationToolbar(canvas1, self)
-        toolbar2 = NavigationToolbar(canvas2, self)
-        toolbar3 = NavigationToolbar(canvas3, self)
-        toolbar4 = NavigationToolbar(canvas4, self)
+        toolbar1 = NavigationToolbar(self.canvas1, self)
+        toolbar2 = NavigationToolbar(self.canvas2, self)
+        toolbar3 = NavigationToolbar(self.canvas3, self)
+        toolbar4 = NavigationToolbar(self.canvas4, self)
         
         mainLayout.addWidget(self.l1, 0, 0)
         mainLayout.addWidget(self.l1_input, 0, 1)
@@ -73,14 +92,22 @@ class Window(QtWidgets.QWidget):
         mainLayout.addWidget(self.l2, 1,0)
         mainLayout.addWidget(self.combo_box, 1, 1)
         mainLayout.addWidget(self.plot_b2, 1, 2)
+        mainLayout.addWidget(self.l3, 2, 2)
+        mainLayout.addWidget(self.l3_output, 2, 3)
+        mainLayout.addWidget(self.l4, 3,2)
+        mainLayout.addWidget(self.l4_output,3,3)
+        mainLayout.addWidget(self.l5, 4,2)
+        mainLayout.addWidget(self.l5_output,4, 3)
+        mainLayout.addWidget(self.l6, 5,2)
+        mainLayout.addWidget(self.l6_output,5, 3)
         mainLayout.addWidget(toolbar1,2,0)
         mainLayout.addWidget(toolbar2,2,1)
         mainLayout.addWidget(toolbar3,4,0)
         mainLayout.addWidget(toolbar4,4,1)
-        mainLayout.addWidget(canvas1,3,0)
-        mainLayout.addWidget(canvas2,3,1)
-        mainLayout.addWidget(canvas3,5,0)
-        mainLayout.addWidget(canvas4,5,1)
+        mainLayout.addWidget(self.canvas1,3,0)
+        mainLayout.addWidget(self.canvas2,3,1)
+        mainLayout.addWidget(self.canvas3,5,0)
+        mainLayout.addWidget(self.canvas4,5,1)
         
         
   
@@ -92,7 +119,7 @@ class Window(QtWidgets.QWidget):
     def openFile(self):
         
         self.filename, _ = QFileDialog.getOpenFileName(self, 'File')
-        #self.l1_input.setText(str(self.filename))
+        self.l1_input.setText(str(self.filename))
         return self.filename
 
         
@@ -100,12 +127,39 @@ class Window(QtWidgets.QWidget):
         self.df = data_import_func.access_file(self.filename)
         
     def plot(self):
+        #Figure 1 is used to plot the time series. 
+        
         self.ax1.clear()
         self.content = self.combo_box.currentText() 
-        self.ax1.plot(self.df[self.content])
-        
+        std_val, mean_val, max_val, min_val = data_import_func.stats(self.df[self.content])
+        self.l3_output.setText('%.6s' % str(mean_val))
+        self.l4_output.setText('%.6s' % str(std_val))
+        self.l5_output.setText('%.6s' % str(max_val))
+        self.l6_output.setText('%.6s' % str(min_val))
+        mean_array = [mean_val] * len(self.df[self.content])
+        self.ax1.plot(self.df[self.content], label = 'Data')
+        self.ax1.plot(self.df.index,mean_array, label = 'Mean', linestyle = '--')
+        self.ax1.set_ylabel('Force [N]')
+        self.ax1.set_xlabel('Time [s]')
+        self.ax1.legend()
+        self.canvas1.draw()
     
-      
+        #Figure 2 will be used to plot the fft
+        spec, freq = data_import_func.spectral_analysis(self.df,self.content)
+        self.ax2.clear()
+        self.ax2.loglog(freq[1:int(len(freq)/2)], spec[1:])
+        self.ax2.set_ylabel('Force [N]')
+        self.ax2.set_xlabel('Frequency [s]')
+        self.canvas2.draw()
+        
+        #Figure 3 will be used to plot a normal distribution of normalised loads
+        self.ax3.clear()
+        self.ax3.hist(self.df[self.content], bins = 50, density=True)
+        self.ax3.set_xlabel('Force [N]')
+        self.ax3.set_ylabel('PDF')
+        sorted_array = self.df.sort_values(by=self.content)[self.content]
+        self.ax3.plot(sorted_array, norm.pdf(sorted_array, mean_val, std_val))
+        self.canvas3.draw()
         
 
         
---
+++ data_import_func.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 09:42:23 2021
+
+@author: goharshoukat
+"""
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy import fftpack
+import math
+import os
+import pickle
+import stats_lib
+#%%
+'''
+File Read into script
+Data Cleaning, Time Stamp Addition
+Simple Statistics Calculated
+Initial Plots
+'''
+def access_file(file, run_time = 360):
+    df = pd.read_csv(file, sep = '\t', skiprows=9, low_memory=False)
+    units = df.iloc[0]
+    df = df.drop([0])
+    #renaming first column header
+    df = df.rename({'#RPM': 'RPM'}, axis=1)
+    units['RPM'] = 'RPM'
+     
+    #all column heads are stored to recreate proper columns
+    col_head = list(df.columns)
+    #the data series is throwing an exception that the data is not numeric
+    #the following for loop converts the entire dataframe to numeric which 
+    #can then be plotted
+    for i in range(len(df.columns)):
+        x = pd.to_numeric(df[col_head[i]])
+        df = df.drop(columns=[col_head[i]])
+        df.insert(i, col_head[i], x)
+        del x
+    
+        
+    #Include time stamps
+    sampling_freq = 256 #Hz
+    t_per_meas = 1/sampling_freq
+    run_time = 360#4160#4097#360#4097# 360#seconds
+    no_of_meas = int(run_time / t_per_meas)
+    # create time arra
+    t = np.linspace(0, run_time, no_of_meas)
+    #first add column to the data framce
+    df['time'] = t
+    #rename index to time values
+    df = df.set_index('time')
+    df['Thrust'] = -df['Thrust']
+    df['Fx'] = -df['Fx']
+    
+    df_new = df[['Thrust', 'Fx1', 'Fx2', 'Fx3', 'Fx']].copy()
+    return df_new
+
+def stats(ser):
+    #Statistics of the data
+    std_val = ser.std(axis = 0)                    # standard deviation
+    mean_val = ser.mean(axis = 0)                  # arithmatic mean
+    max_val = ser.max(axis = 0)                    # max values
+    min_val = ser.min(axis = 0)                    # min values
+    return std_val, mean_val, max_val, min_val
+
+def spectral_analysis(df, column, run_time = 360, sampling_freq = 256):
+    
+    bins = (run_time*sampling_freq)
+    timestep = df.index[1]
+    
+    spec = stats_lib.fft(df[column].to_numpy(), sampling_freq, bins)
+    frequency = np.fft.fftfreq(bins, d=timestep)
+    
+    return spec, frequency
