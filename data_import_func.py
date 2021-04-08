12e2a2a21894df225983e5643e42289f52bff217
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-04-08 16:12:19 +0200

    functions have been adjusted to account for frequency read through the file only.

--- data_import_func.py
+++ data_import_func.py
@@ -14,6 +14,8 @@ import math
 import os
 import pickle
 import stats_lib
+from csaps import csaps
+
 #%%
 '''
 File Read into script
@@ -21,14 +23,15 @@ Data Cleaning, Time Stamp Addition
 Simple Statistics Calculated
 Initial Plots
 '''
-def access_file(file, run_time = 360, sampling_freq = 256):
+def access_file(file):
     df = pd.read_csv(file, sep = '\t', skiprows=9, low_memory=False)
     units = df.iloc[0]
     df = df.drop([0])
     #renaming first column header
-    df = df.rename({'#RPM': 'RPM'}, axis=1)
-    units['RPM'] = 'RPM'
-     
+    try: df = df.rename({'#rpm': 'RPM'}, axis=1)
+    except: df = df.rename({'#RPM': 'RPM'}, axis=1)
+    try: units['#rpm'] = 'RPM'
+    except: units['#RPM'] = 'RPM'
     #all column heads are stored to recreate proper columns
     col_head = list(df.columns)
     #the data series is throwing an exception that the data is not numeric
@@ -40,32 +43,46 @@ def access_file(file, run_time = 360, sampling_freq = 256):
         df.insert(i, col_head[i], x)
         del x
     
-
-    t_per_meas = 1/sampling_freq
-    #4160#4097#360#4097# 360#seconds
-    no_of_meas = int(run_time / t_per_meas)
-    # create time arra
-    t = np.linspace(0, run_time, no_of_meas)
+    sampling_freq = read_freq(file)
+    no_of_meas = len(df)
+       # create time arra
+    t = np.linspace(0, no_of_meas/sampling_freq, no_of_meas)
     #first add column to the data framce
     df['time'] = t
     #rename index to time values
     df = df.set_index('time')
-    df['Thrust'] = -df['Thrust']
-    df['Fx'] = -df['Fx']
-    df['Torque'] = -df['Torque']
-    df['Fy1'] = -df['Fy1']
-    df['Mx1'] = -df['Mx1']
-    df['My1'] = -df['My1']
-    df['Fy2'] = -df['Fy2']
-    df['Mx2'] = -df['Mx2']
-    df['My2'] = -df['My2']
-    df['Fy3'] = -df['Fy3']
-    df['Mx3'] = -df['Mx3']
-    df['My3'] = -df['My3']
-    df['My'] = -df['My']
-    
+    #the following try except statements accomodate for the different column headers
+    try: df['Force'] = -df['Force']
+    except: df['Thrust'] = -df['Thrust']
+    #df['Fx'] = -df['Fx']
+    try: df['Couple'] = -df['Couple']
+    except: df['Torque'] = -df['Torque']
+    '''
+    df['Fy1'] = df['Fy1']
+    df['Mx1'] = df['Mx1']
+    df['My1'] = df['My1']
+    df['Fy2'] = df['Fy2']
+    df['Mx2'] = df['Mx2']
+    df['My2'] = df['My2']
+    df['Fy3'] = df['Fy3']
+    df['Mx3'] = df['Mx3']
+    df['My3'] = df['My3']
+    #df['My'] = -df['My']
+    '''
     
     return df
+
+def read_freq(file):
+    #code to read frequency from file
+    run_parameters = []
+    a_file = open(file)
+    lines_to_read = np.arange(0, 8, 1)
+
+    for position, line in enumerate(a_file):
+        if position in lines_to_read:
+            run_parameters.append(line)
+
+    return (int(run_parameters[3][-9:-6]))
 
 def stats(ser):
     #Statistics of the data
@@ -75,18 +92,18 @@ def stats(ser):
     min_val = ser.min(axis = 0)                    # min values
     return std_val, mean_val, max_val, min_val
 
-def spectral_analysis(df, column, run_time = 360, sampling_freq = 256, bins = False):
+def spectral_analysis(df, column, bins = False):
     
     if bins == 'Default':
         
-        bins = int(run_time*sampling_freq)
+        bins = len(df)
         timestep = df.index[1]
     else:
         timestep = df.index[1]
-    
+    sampling_freq = 1/timestep
     spec = stats_lib.fft(df[column].to_numpy(), int(sampling_freq), int(bins))
     frequency = np.fft.fftfreq(int(bins), d=timestep)
-    
+
     return spec, frequency
 
 #function to cacluate rotor frequency to normalise fft
@@ -115,6 +132,7 @@ def filtrage(t,x,fc,filter_type):
     xf = np.real(np.fft.ifft(fftx))
     return xf
 # %% Anglue Turbine
+
 def angle_turbine(t, Fy1, Fy2, Fy3, fr):
         
     f1 = fr - 0.2
@@ -130,5 +148,42 @@ def angle_turbine(t, Fy1, Fy2, Fy3, fr):
     theta3=np.unwrap(np.arctan2((-np.sqrt(3)*(Fy2+Fy3)),(Fy2-Fy3)))
     theta = np.mean(np.column_stack([theta1, theta2, theta3]), axis = 1)
     theta=theta-np.round(theta/(2*np.pi))*(2*np.pi)
-    theta = theta * 180/np.pi + 180
-    return theta
+    theta1=theta1-np.round(theta1/(2*np.pi))*(2*np.pi)
+    theta2=theta2-np.round(theta2/(2*np.pi))*(2*np.pi)
+    theta3=theta3-np.round(theta3/(2*np.pi))*(2*np.pi)
+    df = pd.DataFrame({'Fy1': Fy1, 'Fy2':Fy2, 'Fy3':Fy3, 'theta1':theta1, 'theta2':theta2, 'theta3':theta3, 'theta':theta})
+    df.index = t
+    return df
+
+def angle_theta(df, content):
+    t = df.index
+    Fy1 = df['Fy1'].to_numpy()
+    Fy2 = df['Fy2'].to_numpy()
+    Fy3 = df['Fy3'].to_numpy()
+    fr = rotor_freq(df['RPM'])
+    angle_theta = angle_turbine(t, Fy1, Fy2, Fy3, fr)
+    #plt.scatter(angle_theta['theta'], df[content], s= 0.01)
+    newdf = pd.DataFrame({'theta':angle_theta['theta'], 'signal': df[content]})
+    
+    angle_df_sorted = newdf.sort_values(by = 'theta')
+    theta_s = np.arange(-np.pi, np.pi, np.pi/len(newdf))
+    fx_s = csaps(angle_df_sorted['theta'], angle_df_sorted['signal'], theta_s, smooth = .95)
+    #plt.plot(theta_s, fx_s)
+    fit_df = pd.DataFrame({'theta_s': theta_s, 'Average': fx_s})
+    return angle_df_sorted, fit_df
+
+# %% Calculation of Mean at each degree
+
+def fitting(signal_, theta, plot = False):
+    
+    theta_s = np.arange(-np.pi, np.pi, np.pi/len(signal_))
+    newdf= pd.DataFrame({'theta': theta, 'signal': signal_})
+    newdf = newdf.sort_values(by = 'theta')
+    avg = csaps(theta, signal, theta_s, smooth = .95)
+    
+    if plot == True:
+        plt.scatter(np.rad2deg(newdf['theta']), newdf['signal'], s =0.01)
+        plt.plot(np.rad2deg(theta_s), avg)
+        plt.xlabel('time[s]')
+        #plt.ylabel()
+    return avg, theta_s
