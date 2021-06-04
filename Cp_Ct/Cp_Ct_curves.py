de3be063ef56565ea2660c6fcac276b21f70a454
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2021-06-04 22:15:30 +0200

    Standard deviation calculated by finding Cp time signal.

--- Cp_Ct/Cp_Ct_curves.py
+++ Cp_Ct/Cp_Ct_curves.py
@@ -9,8 +9,8 @@ Created on Sun May 16 15:25:58 2021
 import numpy as np
 import pandas as pd
 import os
-from load_ldv3D import load_ldv3D
-from data_import_func import access_file
+from Dashboard.load_ldv3D import load_ldv3D
+from Dashboard.data_import_func import access_file
 import math
 import matplotlib as mpl
 mpl.rcParams.update(mpl.rcParamsDefault)
@@ -29,23 +29,43 @@ force_files = np.sort(os.listdir(path_data_files))[2:169] #sorting + deletion of
 
 
 #first create a velocity data frame with only the details of the files
-vel_df= pd.DataFrame({'Original Name':dfs['Original'],'LDV Name':dfs['LDV File Name'], 'U_mean' : None, 'U^2_mean':None, 'U^3_mean':None})
-
-
+#add standard deviation as well to study the results
+vel_df= pd.DataFrame({'Original Name':dfs['Original'],'LDV Name':dfs['LDV File Name'], \
+                      'U_mean' : None, 'U^2_mean' : None, 'U^3_mean' : None, \
+                    'Sigma_U': None, 'Sigma_U^2' : None, 'Sigma_U^3' : None})
 #add the U**2 and U**3 columns to this DF
 #these are the measured values. The velocity recorded by the ldv incident on the turbine fluctuates. 
 for file in ldv_files:
     t, u = load_ldv3D(path_ldv_files+file, 0)
     u2 = np.mean(u**2)
     u3 = np.mean(u**3)
-    u = np.mean(u)
-    vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'U_mean'] = u
+    ubar = np.mean(u)
+    sigmaU = np.std(u)
+    sigmaU2 = np.std(u**2)
+    sigmaU3 = np.std(u**3)
+    vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'U_mean'] = ubar
     vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'U^2_mean'] = u2
     vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'U^3_mean'] = u3
+    vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'Sigma_U'] = sigmaU
+    vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'Sigma_U^2'] = sigmaU2
+    vel_df.at[vel_df[vel_df['LDV Name']==file[:-4]].index[0], 'Sigma_U^3'] = sigmaU3
     
-    
+#rename columns to match the original data file
+vel_df = vel_df.rename(columns = {'Original Name':'Original', 'LDV Name':'LDV File Name'})
+#merge the velocity columns
+dfs = pd.merge(dfs, vel_df[['Original', 'U_mean', 'U^2_mean', 'U^3_mean', 'Sigma_U', 'Sigma_U^2', 'Sigma_U^3']], on = 'Original', how = 'left')
+
 #creaet a data frame with average of the thrust and RPM from the data files
-force_df = pd.DataFrame({'Original Name':dfs['Original'], 'Force Name':dfs['Force File Name'], 'RPM' : None, 'Thrust_mean' : None, 'Torque_mean' : None})
+force_df = pd.DataFrame({'Original Name':dfs['Original'], 'Force Name':dfs['Force File Name'], \
+                         'RPM' : None, 'Thrust_mean' : None, 'Torque_mean' : None, \
+                        'Thrust sigma' : None, 'Torque sigma' : None, 'Cp mean' : None, \
+                            'Cp sigma' : None, 'Ct mean' : None, 'Ct sigma' : None})
+
+#Cp curve
+#Eq: Cp = Torque x RPM / 0.5 x rho x A x u^3_mean
+rho = 1000 #water density in kg/m3
+rotor_dia = 0.724
+rotor_area = math.pi * rotor_dia**2 / 4
 for file in force_files:
     #reading the entire data file can slow down the process
     #in future, this functionality can be adjusted
@@ -54,32 +74,34 @@ for file in force_files:
     rpm_mean = np.mean(df['RPM']) 
     thrust_mean = np.mean(df['Force'])
     torque_mean = np.mean(df['Couple'])
+    thrust_std = np.std(df['Force'])
+    torque_std  = np.std(df['Couple'])
     force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'RPM'] = rpm_mean
     force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Thrust_mean'] = thrust_mean
     force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Torque_mean'] = torque_mean
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Torque sigma'] = torque_std
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Thrust sigma'] = thrust_std
+    
+    
+    #calculate instantenous Cp and Ct
+    Cp =  df['Couple'] * df['RPM'] * 2 * np.pi/60 / float(0.5 * rho * \
+                                                rotor_area * dfs[dfs['Force File Name'] == file[:-4]]['U^3_mean'])
+    Ct = df['Force'] / float(0.5 * rho * rotor_area * dfs[dfs['Force File Name'] == file[:-4]]['U^2_mean'])
+    
+    
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Cp mean'] = np.mean(Cp)
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Cp sigma'] = np.std(Cp)
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Ct mean'] = np.mean(Ct)
+    force_df.at[force_df[force_df['Force Name']==file[:-4]].index[0], 'Ct sigma'] = np.std(Ct)
+    
 
 
 #rename columns to match the original data file
-vel_df = vel_df.rename(columns = {'Original Name':'Original', 'LDV Name':'LDV File Name'})
 force_df = force_df.rename(columns = {'Original Name':'Original'})
 #merge the velocity columns
-dfs = pd.merge(dfs, vel_df[['Original', 'U_mean', 'U^2_mean', 'U^3_mean']], on = 'Original', how = 'left')
-dfs = pd.merge(dfs, force_df[['Original', 'RPM', 'Thrust_mean', 'Torque_mean']], on = 'Original', how = 'left')
+dfs = pd.merge(dfs, force_df[['Original', 'RPM', 'Thrust_mean', 'Torque_mean', 'Torque sigma', 'Thrust sigma', 'Cp mean', 'Cp sigma', 'Ct mean', 'Ct sigma']], on = 'Original', how = 'left')
+
 dfs.to_csv('Cp_Ct/Experiment_Summary.csv')
-
-
-#write the new csv with the average values to avoid recalculating
-#Cp curve
-#Eq: Cp = Torque x RPM / 0.5 x rho x A x u^3_mean
-rho = 1000 #water density in kg/m3
-rotor_dia = 0.724
-rotor_area = math.pi * rotor_dia**2 / 4
-dfs['Cp'] = dfs['Torque_mean'] * dfs['RPM'] * 2 * np.pi/60 / (0.5 * rho * \
-                                                rotor_area * dfs['U^3_mean'])
-#Ct calculation
-# Thrust / o.5 x rho x A x U**2_mean
-dfs['Ct'] = dfs['Thrust_mean']  / (0.5 * rho * rotor_area * dfs['U^2_mean'])
-
 
 #create seperate dataframes depending on the positions
 #the below calculation can take place through simple function. 
@@ -91,42 +113,57 @@ path_performance_curves = 'Results/Performance Coefficients/'
 pxxxv08 = dfs.fillna('xxx')
 pxxxv08['TSR'] = pxxxv08['TSR'].str.replace(',','.')
 pxxxv08 = pxxxv08[(pxxxv08['Force File Name'].str.contains(r'PXXX')) & (pxxxv08['Velocity']=='0,8')]
+#Cp_errorxxxv08 = [pxxxv08['Cp mean'] - pxxxv08['Cp sigma'], pxxxv08['Cp mean'] - pxxxv08['Cp sigma']]
+#Ct_errorxxxv08 = [pxxxv08['Ct mean'] - pxxxv08['Ct sigma'], pxxxv08['Ct mean'] - pxxxv08['Ct sigma']]
+Cp_errorxxxv08 = pxxxv08['Cp sigma']
+Ct_errorxxxv08 = pxxxv08['Ct sigma']
 
 
 #velocity 1 m/s
 pxxxv10 = dfs.fillna('xxx')
 pxxxv10['TSR'] = pxxxv10['TSR'].str.replace(',','.')
 pxxxv10 = pxxxv10[(pxxxv10['Force File Name'].str.contains(r'PXXX')) & (pxxxv10['Velocity']=='1')].drop(183) #drop the 183rd index which is the long run
-    
+#Cp_errorxxxv10 = [pxxxv10['Cp mean'] - pxxxv10['Cp sigma'], pxxxv10['Cp mean'] - pxxxv10['Cp sigma']]
+#Ct_errorxxxv10 = [pxxxv10['Ct mean'] - pxxxv10['Ct sigma'], pxxxv10['Ct mean'] - pxxxv10['Ct sigma']]
+Cp_errorxxxv10 = pxxxv10['Cp sigma']
+Ct_errorxxxv10 = pxxxv10['Ct sigma']
+
+
 #velocity 1.2 m/s
 pxxxv12 = dfs.fillna('xxx')
 pxxxv12['TSR'] = pxxxv12['TSR'].str.replace(',','.')
 pxxxv12 = pxxxv12[(pxxxv12['Force File Name'].str.contains(r'PXXX')) & (pxxxv12['Velocity']=='1,2')]
+#Cp_errorxxxv12 = [pxxxv12['Cp mean'] - pxxxv12['Cp sigma'], pxxxv12['Cp mean'] - pxxxv12['Cp sigma']]
+#Ct_errorxxxv12 = [pxxxv12['Ct mean'] - pxxxv12['Ct sigma'], pxxxv12['Ct mean'] - pxxxv12['Ct sigma']]
+Cp_errorxxxv12 = pxxxv12['Cp sigma']
+Ct_errorxxxv12 =  pxxxv12['Ct sigma']
+
 
 
 #plot and save Cp
 plt.xlabel('TSR')
 plt.ylabel('$C_P$')
-plt.plot((pxxxv08['TSR'].astype(float)),(pxxxv08['Cp'].astype(float)), label = 'Velocity = 0.8 m/s')
-plt.plot((pxxxv10['TSR'].astype(float)),(pxxxv10['Cp'].astype(float)), label = 'Velocity = 1.0 m/s')
-plt.plot((pxxxv12['TSR'].astype(float)),(pxxxv12['Cp'].astype(float)), label = 'Velocity = 1.2 m/s')
+plt.errorbar((pxxxv08['TSR'].astype(float)),(pxxxv08['Cp mean'].astype(float)), yerr= Cp_errorxxxv08, label = 'Velocity = 0.8 m/s')
+plt.errorbar((pxxxv10['TSR'].astype(float)),(pxxxv10['Cp mean'].astype(float)), yerr= Cp_errorxxxv10, label = 'Velocity = 1.0 m/s')
+plt.errorbar((pxxxv12['TSR'].astype(float)),(pxxxv12['Cp mean'].astype(float)), yerr= Cp_errorxxxv12, label = 'Velocity = 1.2 m/s')
 plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_p PXXX.png')
-
+plt.close()
 
 #plot and save Ct
 plt.xlabel('TSR')
 plt.ylabel('$C_T$')
-plt.plot((pxxxv08['TSR'].astype(float)),(pxxxv08['Ct'].astype(float)), label = 'Velocity = 0.8 m/s')
-plt.plot((pxxxv10['TSR'].astype(float)),(pxxxv10['Ct'].astype(float)), label = 'Velocity = 1.0 m/s')
-plt.plot((pxxxv12['TSR'].astype(float)),(pxxxv12['Ct'].astype(float)), label = 'Velocity = 1.2 m/s')
+plt.errorbar((pxxxv08['TSR'].astype(float)),(pxxxv08['Ct mean'].astype(float)), yerr= Ct_errorxxxv08, label = 'Velocity = 0.8 m/s')
+plt.errorbar((pxxxv10['TSR'].astype(float)),(pxxxv10['Ct mean'].astype(float)), yerr= Ct_errorxxxv10, label = 'Velocity = 1.0 m/s')
+plt.errorbar((pxxxv12['TSR'].astype(float)),(pxxxv12['Ct mean'].astype(float)), yerr= Ct_errorxxxv12, label = 'Velocity = 1.2 m/s')
+plt.legend(loc = 'lower right')
 plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_t PXXX.png')
-
+plt.close()
 # %% DataFrame for Position = 594mm
 #velocity 0.8 m/s
 p594v08 = dfs.fillna('xxx')
@@ -155,7 +192,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_p P594.png')
-
+plt.close()
 
 #plot and save Ct
 plt.xlabel('TSR')
@@ -167,7 +204,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_t P594.png')
-
+plt.close()
 # %% DataFrame for Position = 427mm
 #velocity 0.8 m/s
 p427v08 = dfs.fillna('xxx')
@@ -196,7 +233,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_p P427.png')
-
+plt.close()
 
 #plot and save Ct
 plt.xlabel('TSR')
@@ -208,7 +245,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_t P427.png')
-
+plt.close()
 
 # %% DataFrame for Position = 260 mm
 #velocity 0.8 m/s
@@ -238,7 +275,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_p P260.png')
-
+plt.close()
 
 #plot and save Ct
 plt.xlabel('TSR')
@@ -250,7 +287,7 @@ plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_t P260.png')
-
+plt.close()
 # %% DataFrame for Position = 93 mm
 #velocity 0.8 m/s
 p093v08 = dfs.fillna('xxx')
@@ -273,20 +310,126 @@ plt.xlabel('TSR')
 plt.ylabel('$C_P$')
 plt.plot((p093v08['TSR'].astype(float)),(p093v08['Cp'].astype(float)), label = 'Velocity = 0.8 m/s')
 plt.plot((p093v10['TSR'].astype(float)),(p093v10['Cp'].astype(float)), label = 'Velocity = 1.0 m/s')
-#plt.plot((p093v12['TSR'].astype(float)),(p093v12['Cp'].astype(float)), label = 'Velocity = 1.2 m/s')
+plt.plot((p093v12['TSR'].astype(float)),(p093v12['Cp'].astype(float)), label = 'Velocity = 1.2 m/s')
 plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_p P093.png')
-
+plt.close()
 
 #plot and save Ct
 plt.xlabel('TSR')
 plt.ylabel('$C_T$')
 plt.plot((p093v08['TSR'].astype(float)),(p093v08['Ct'].astype(float)), label = 'Velocity = 0.8 m/s')
 plt.plot((p093v10['TSR'].astype(float)),(p093v10['Ct'].astype(float)), label = 'Velocity = 1.0 m/s')
-#plt.plot((p093v12['TSR'].astype(float)),(p093v12['Ct'].astype(float)), label = 'Velocity = 1.2 m/s')
+plt.plot((p093v12['TSR'].astype(float)),(p093v12['Ct'].astype(float)), label = 'Velocity = 1.2 m/s')
 plt.legend(loc = 'lower right')
 plt.grid(True)
 plt.show()
 plt.savefig(path_performance_curves + 'C_t P093.png')
+plt.close()
+# %% 
+
+# %% read the csv including the indication for error files
+dfs2 = pd.read_csv('Cp_Ct/Experiment_Summary.csv', sep = ';')
+dfs2['Error_File'] = dfs2['Error_File'].str.replace(',00','')
+
+# %% Plotting graphs with focus on positions
+v08 = dfs2.fillna('xxx')
+v08['TSR'] = v08['TSR'].str.replace(',','.')
+v08 = (v08[v08['Velocity']=='0,8'].drop(85))
+ev08Pxxx = [v08[v08['Reletive Position'] == 'xxx']['Cp +'] - v08[v08['Reletive Position']=='xxx']['Cp'].astype(float), \
+           v08[v08['Reletive Position']=='xxx']['Cp'].astype(float) - v08[v08['Reletive Position'] == 'xxx']['Cp -']]
+#plot and save Cp for velocity of v=0.8m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_P$')
+plt.plot(v08[v08['Reletive Position']=='xxx']['TSR'].astype(float), v08[v08['Reletive Position']=='xxx']['Cp'].astype(float), '-D', markevery=[3], label = 'Without Mast')
+plt.plot(v08[v08['Reletive Position']=='594,000']['TSR'].astype(float), v08[v08['Reletive Position']=='594,000']['Cp'].astype(float), '-D', markevery=[3], label = '594 mm')
+plt.plot(v08[v08['Reletive Position']=='427,000']['TSR'].astype(float), v08[v08['Reletive Position']=='427,000']['Cp'].astype(float), '-D', markevery=[3], label = '427 mm')
+plt.plot(v08[v08['Reletive Position']=='260,000']['TSR'].astype(float), v08[v08['Reletive Position']=='260,000']['Cp'].astype(float), '-D', markevery=[3],label = '260 mm')
+plt.plot(v08[v08['Reletive Position']=='93,000']['TSR'].astype(float), v08[v08['Reletive Position']=='93,000']['Cp'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_p V0.8.png')
+plt.close()
+#plot and save Cp for velocity of v=0.8m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_t$')
+plt.plot(v08[v08['Reletive Position']=='xxx']['TSR'].astype(float), v08[v08['Reletive Position']=='xxx']['Ct'].astype(float), '-D', markevery=[3], label = 'Without Mast')
+plt.plot(v08[v08['Reletive Position']=='594,000']['TSR'].astype(float), v08[v08['Reletive Position']=='594,000']['Ct'].astype(float), '-D', markevery=[3], label = '594 mm')
+plt.plot(v08[v08['Reletive Position']=='427,000']['TSR'].astype(float), v08[v08['Reletive Position']=='427,000']['Ct'].astype(float), '-D', markevery=[3], label = '427 mm')
+plt.plot(v08[v08['Reletive Position']=='260,000']['TSR'].astype(float), v08[v08['Reletive Position']=='260,000']['Ct'].astype(float), '-D', markevery=[3],label = '260 mm')
+plt.plot(v08[v08['Reletive Position']=='93,000']['TSR'].astype(float), v08[v08['Reletive Position']=='93,000']['Ct'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_t V0.8.png')
+plt.close()
+# %% Velocity 1.0 m/s
+v10 = dfs2.fillna('xxx')
+v10['TSR'] = v10['TSR'].str.replace(',','.')
+#drop the rows without TSR sweep information and longrun 
+v10 = v10[v10['Velocity']=='1'].drop(np.arange(160, 170, 1)).drop([73, 123, 183])
+
+
+#plot and save Cp for velocity of v=1 m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_P$')
+plt.plot(v10[v10['Reletive Position']=='xxx']['TSR'].astype(float), v10[v10['Reletive Position']=='xxx']['Cp'].astype(float), '-D', markevery=[3], label = 'Without Mast')
+plt.plot(v10[v10['Reletive Position']=='594,000']['TSR'].astype(float), v10[v10['Reletive Position']=='594,000']['Cp'].astype(float),  '-D', markevery=[3], label = '594 mm')
+plt.plot(v10[v10['Reletive Position']=='427,000']['TSR'].astype(float), v10[v10['Reletive Position']=='427,000']['Cp'].astype(float),  '-D', markevery=[3], label = '427 mm')
+plt.plot(v10[v10['Reletive Position']=='260,000']['TSR'].astype(float), v10[v10['Reletive Position']=='260,000']['Cp'].astype(float),  '-D', markevery=[3], label = '260 mm')
+plt.plot(v10[v10['Reletive Position']=='93,000']['TSR'].astype(float), v10[v10['Reletive Position']=='93,000']['Cp'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_p V1.0.png')
+plt.close()
+
+#plot and save Cp for velocity of v =1 m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_t$')
+plt.plot(v10[v10['Reletive Position']=='xxx']['TSR'].astype(float), v10[v10['Reletive Position']=='xxx']['Ct'].astype(float), '-D', markevery=[3], label = 'Without Mast')
+plt.plot(v10[v10['Reletive Position']=='594,000']['TSR'].astype(float), v10[v10['Reletive Position']=='594,000']['Ct'].astype(float),  '-D', markevery=[3], label = '594 mm')
+plt.plot(v10[v10['Reletive Position']=='427,000']['TSR'].astype(float), v10[v10['Reletive Position']=='427,000']['Ct'].astype(float),  '-D', markevery=[3], label = '427 mm')
+plt.plot(v10[v10['Reletive Position']=='260,000']['TSR'].astype(float), v10[v10['Reletive Position']=='260,000']['Ct'].astype(float),  '-D', markevery=[3], label = '260 mm')
+plt.plot(v10[v10['Reletive Position']=='93,000']['TSR'].astype(float), v10[v10['Reletive Position']=='93,000']['Ct'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_t V1.0.png')
+plt.close()
+# %% Velocity 1.2 m/s
+v12 = dfs2.fillna('xxx')
+v12['TSR'] = v12['TSR'].str.replace(',','.')
+#drop the rows without TSR sweep information and longrun 
+v12 = v12[v12['Velocity']=='1,2']
+
+#plot and save Cp for velocity of v=1.2 m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_P$')
+plt.plot(v12[v12['Reletive Position']=='xxx']['TSR'].astype(float), v12[v12['Reletive Position']=='xxx']['Cp'].astype(float), '-D', markevery=[3,4], label = 'Without Mast')
+plt.plot(v12[v12['Reletive Position']=='594,000']['TSR'].astype(float), v12[v12['Reletive Position']=='594,000']['Cp'].astype(float),  '-D', markevery=[3, 4], label = '594 mm')
+plt.plot(v12[v12['Reletive Position']=='427,000']['TSR'].astype(float), v12[v12['Reletive Position']=='427,000']['Cp'].astype(float),  '-D', markevery=[3], label = '427 mm')
+plt.plot(v12[v12['Reletive Position']=='260,000']['TSR'].astype(float), v12[v12['Reletive Position']=='260,000']['Cp'].astype(float),  '-D', markevery=[3], label = '260 mm')
+plt.plot(v12[v12['Reletive Position']=='93,000']['TSR'].astype(float), v12[v12['Reletive Position']=='93,000']['Cp'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_p V1.2.png')
+plt.close()
+
+#plot and save Cp for velocity of v =1.2 m/s
+plt.xlabel('TSR')
+plt.ylabel('$C_t$')
+plt.plot(v12[v12['Reletive Position']=='xxx']['TSR'].astype(float), v12[v12['Reletive Position']=='xxx']['Ct'].astype(float), '-D', markevery=[3, 4], label = 'Without Mast')
+plt.plot(v12[v12['Reletive Position']=='594,000']['TSR'].astype(float), v12[v12['Reletive Position']=='594,000']['Ct'].astype(float),  '-D', markevery=[3, 4], label = '594 mm')
+plt.plot(v12[v12['Reletive Position']=='427,000']['TSR'].astype(float), v12[v12['Reletive Position']=='427,000']['Ct'].astype(float),  '-D', markevery=[3], label = '427 mm')
+plt.plot(v12[v12['Reletive Position']=='260,000']['TSR'].astype(float), v12[v12['Reletive Position']=='260,000']['Ct'].astype(float),  '-D', markevery=[3], label = '260 mm')
+plt.plot(v12[v12['Reletive Position']=='93,000']['TSR'].astype(float), v12[v12['Reletive Position']=='93,000']['Ct'].astype(float), label = '93 mm')
+plt.legend(loc = 'lower right')
+plt.grid(True)
+plt.show()
+plt.savefig(path_performance_curves + 'C_t V1.2.png')
+plt.close()
